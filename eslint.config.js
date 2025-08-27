89aac8744f9c9df612f2150c1848754d1a55489b
LeonID <lmatskevich79@gmail.com>
2025-08-27 23:53:25 +0300

    feat: add Prettier configuration and scripts

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,42 @@
+// eslint.config.js
 import js from '@eslint/js'
 import globals from 'globals'
+import ts from 'typescript-eslint'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      prettier,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-])
+]
